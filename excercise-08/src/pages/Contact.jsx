76e09f052754a6b11d8a751a0d14bf426ff2a4d9
Agustín Comolli <agustincomolli@gmail.com>
2025-05-09 12:20:11 -0300

    feat: add SweetAlert2 for form submission feedback in Contact page

--- excercise-08/src/pages/Contact.jsx
+++ excercise-08/src/pages/Contact.jsx
@@ -1,6 +1,21 @@
+import Swal from 'sweetalert2';
 import styles from "./Contact.module.css"
 
 export default function Contact({ showCart, cartItemsList, setCartItemsList }) {
+  function handleSubmit(event) {
+    event.preventDefault()
+    const message = "<p>Su mensaje ha sido enviado correctamente.</p>"
+      + "<p>Nos comunicaremos con usted a la brevedad.</p>"
+   
+    Swal.fire({
+      title: "Información de contacto",
+      html: message,
+      icon: "success",
+      confirmButtonText: "Continuar",
+      footer:"<p>Muchas gracias</p>"
+    })
+  };
+
   return (
     <div className="pageContent">
       <h2>Contáctanos</h2>
@@ -9,15 +24,15 @@ export default function Contact({ showCart, cartItemsList, setCartItemsList }) {
         Completa el siguiente formulario y te responderemos lo antes posible.
       </p>
 
-      <form className={styles.contactForm}>
+      <form className={styles.contactForm} onSubmit={handleSubmit}>
         <label htmlFor="user-name">Nombre:</label>
-        <input type="text" id="user-name" name="user-name" required />
+        <input type="text" id="user-name" name="user-name" required placeholder="Nombre y Apellido" />
 
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" name="email" required />
+        <input type="email" id="email" name="email" required placeholder="alguien@dominio.com" />
 
         <label htmlFor="message">Mensaje:</label>
-        <textarea id="message" name="message" rows="8" required></textarea>
+        <textarea id="message" name="message" rows="8" required placeholder="Escribe tu mensaje aquí"></textarea>
 
         <button className="btn btn-primary" type="submit">Enviar Mensaje</button>
       </form>
